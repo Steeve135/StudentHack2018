35c22b728e4198b4b216d8c6566b2df9f56ce641
lucasDF99 <lucas.diaz.fiol@gmail.com>
2018-04-29 11:07:39 +0100

    putting it all together

--- StudentHackQuiz/app/src/main/java/com/example/lucas/studenthackquiz/QuizActivity.java
+++ StudentHackQuiz/app/src/main/java/com/example/lucas/studenthackquiz/QuizActivity.java
@@ -11,7 +11,7 @@ import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class QuizActivity extends AppCompatActivity {
+public class QuizActivity extends MainActivity {
 
     private QuestionLibrary possibleQuestions = new QuestionLibrary();
 
